2dd16266c1c3168068f088154052434dc3200fec
Sergei Baikin <gdx@yandex.com>
2022-07-14 08:49:05 +0200

    Laravel add processors (#1)
    * Add custom processors
    
    * Update documentation
    
    * Up composer version
    
    Co-authored-by: sergei.baikin <sergei.baikin@fotograf.de>

--- src/Laravel/LaravelLoggerCreating.php
+++ src/Laravel/LaravelLoggerCreating.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Gotphoto\Logging\Laravel;
 
+use Aws\Exception\AwsException;
+use Gotphoto\Logging\ExceptionContext\AwsExceptionContext;
+use Gotphoto\Logging\ExceptionContext\GuzzleRequestExceptionContext;
 use Gotphoto\Logging\Formatter;
+use GuzzleHttp\Exception\RequestException;
 use Illuminate\Support\Facades\App;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
@@ -16,18 +20,28 @@ final class LaravelLoggerCreating
     {
         assert(!empty($config['app_name']) && is_string($config['app_name']));
         assert(!empty($config['channel']) && is_string($config['channel']));
+        assert(!isset($config['processors']) || is_array($config['processors']));
         $appName = $config['app_name'];
         $channel = $config['channel'];
+        /** @var \Monolog\Processor\ProcessorInterface[] $processors */
+        $processors = $config['processors'] ?? [];
 
         $log = new Logger($channel);
         $log->pushProcessor(new Processor());
+
+        foreach ($processors as $processor) {
+            $log->pushProcessor($processor);
+        }
 
         $streamHandler = new StreamHandler('php://stderr');
 
         $handler = $streamHandler;
         $env     = App::environment();
         $handler->setFormatter(
-            new Formatter($appName, (is_string($env) ? $env : "undefined"), [])
+            new Formatter($appName, (is_string($env) ? $env : "undefined"), [
+                RequestException::class => [new GuzzleRequestExceptionContext()],
+                AwsException::class     => [new AwsExceptionContext()],
+            ])
         );
         $log->pushHandler($handler);
 
